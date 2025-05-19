b625368d7805640b82c2960a0b5177a75000044b
ArtMages <dimakolesn5@gmail.com>
2025-05-19 09:59:38 +0200

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int SIZE = 8;
+    int n[SIZE];
+
+    std::cout << "Введіть 8 чисел:\n";
+    for (int i = 0; i < SIZE; i = i + 1) {
+        std::cout << "Число " << i + 1 << ": ";
+        std::cin >> n[i];
+    }
+
+    for (int i = 0; i < SIZE - 1; i = i + 1) {
+        for (int j = 0; j < SIZE - i - 1; j = j + 1) {
+            if (n[j] > n[j + 1]) {
+                int temp = n[j];
+                n[j] = n[j + 1];
+                n[j + 1] = temp;
+            }
+        }
+    }
+
+    std::cout << "\nВідсортовані числа (від найменшого до найбільшого):\n";
+    for (int i = 0; i < SIZE; i = i + 1) {
+        std::cout << n[i] << " ";
+    }
+
+    std::cout << std::endl;
+    return 0;
+}
